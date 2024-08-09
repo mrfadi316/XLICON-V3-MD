15ac69cbbcb6cdc221bcb0cdab91ec130411c94c
Mr Fadi <100305289+mrfadi316@users.noreply.github.com>
2024-08-09 22:54:55 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -52,13 +52,13 @@ global.location = process.env.GL_INFO || "Pakistan, Lahore, Sabzazar" //ur locat
 //________________________________________________________________________________________________________
 global.botname = process.env.BOT_NAME || 'XLICON-V3-MD' //ur bot name
 //________________________________________________________________________________________________________
-global.ownernumber = process.env.SUDO || '33644629071' //ur owner number
+global.ownernumber = process.env.SUDO || '972585265663' //ur owner number
 //________________________________________________________________________________________________________
 global.ownername = process.env.SUDO_NAME || 'Mr Fadi' //ur owner name
 //________________________________________________________________________________________________________
 global.websitex = process.env.YT_LINK || "https://youtube.com/@mrfadi316"
 //________________________________________________________________________________________________________
-global.wagc = process.env.GL_PUSH || "https://whatsapp.com/channel/0029VadqnPO2phHMssyWQ300"
+global.wagc = process.env.GL_PUSH || "https://whatsapp.com/channel/0029VagMsjy5vKA5InUg2S10"
 //________________________________________________________________________________________________________
 global.themeemoji = process.env.GL_EMOJI || '🏮'
 //________________________________________________________________________________________________________
